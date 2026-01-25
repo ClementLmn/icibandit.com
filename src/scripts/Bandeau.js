f3c29fa6869c350004105d995ec18cdf3c9d268a
Clément Lemoine <clementlemoine.cl@gmail.com>
2026-01-25 16:47:33 +0100

    ✨bandeaux + slanted sections

---
+++ src/scripts/Bandeau.js
@@ -0,0 +1,53 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export default class Bandeau {
+    constructor(el) {
+        this.el = el;
+        this.inner = el.querySelector(".bandeau-inner");
+        this.direction = this.el.getAttribute("data-direction") || "left";
+        this.width = this.inner.clientWidth / 2;
+        this.initAnimation();
+    }
+
+    initAnimation() {
+        this.wrap = gsap.utils.wrap(-this.width, 0);
+        this.tl = gsap.timeline({
+            repeat: -1,
+        });
+
+        this.tl.to(this.inner, {
+            x:
+                this.direction === "left"
+                    ? `-=${this.width}`
+                    : `+=${this.width}`,
+            ease: "linear",
+            duration: 10,
+            modifiers: {
+                x: gsap.utils.unitize(this.wrap),
+            },
+        });
+
+        this.speed = ScrollTrigger.create({
+            trigger: this.el,
+            start: "top bottom",
+            end: "bottom top",
+            onUpdate: (self) => {
+                const speedMultiplier = 1000;
+                const velocity = Math.abs(self.getVelocity()) / speedMultiplier;
+                this.tl.timeScale(1 + velocity);
+            },
+        });
+    }
+
+    onResize() {
+        const newWidth = this.inner.clientWidth / 2;
+        if (newWidth === this.width) return;
+        this.width = this.inner.clientWidth / 2;
+        this.wrap = gsap.utils.wrap(-this.width, 0);
+        this.tl.kill();
+        this.speed.kill();
+        this.initAnimation();
+    }
+}
